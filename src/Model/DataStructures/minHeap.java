066bb63260ea5608a655fc5850857d47e66d2abb
MMattLeeE <MMattLeeE@gmail.com>
2017-07-24 04:10:15 -0400

    .

--- src/Model/DataStructures/minHeap.java
+++ src/Model/DataStructures/minHeap.java
@@ -6,12 +6,12 @@ import java.util.ArrayList;
  * Created by Matt on 7/18/2017.
  */
 public class minHeap<E extends Comparable<E>> implements IPriorityQueue<E> {
-    private ArrayList<E> elements;
+    private ArrayList<E> elementsArray;
     private int lastIndex;
     private int maxIndex;
 
     public minHeap(int maxSize) {
-        elements = new ArrayList<>(maxSize);
+        elementsArray = new ArrayList<>(maxSize);
         lastIndex = -1;
         maxIndex = maxSize-1;
     }
@@ -33,10 +33,19 @@ public class minHeap<E extends Comparable<E>> implements IPriorityQueue<E> {
         else
         {
             lastIndex++;
-            elements.add(lastIndex, element);
+            elementsArray.add(lastIndex, element);
             reheapUp(element);
         }
     }
+    private void reheapUp(E element) {
+        int hole = lastIndex;
+        while ((hole > 0) && (element.compareTo(elementsArray.get((hole - 1) / 2)) < 0)) {
+            elementsArray.set(hole, elementsArray.get((hole - 1) / 2));   // move hole's parent down
+            hole = (hole - 1) / 2;                             // move hole up
+        }
+        elementsArray.set(hole, element);                   // place element into final hole
+    }
+
 
     @Override
     public E dequeue() throws PriorityQueueUnderflowException {
@@ -46,37 +55,25 @@ public class minHeap<E extends Comparable<E>> implements IPriorityQueue<E> {
             throw new PriorityQueueUnderflowException("Priority queue is empty");
         else
         {
-            hold = elements.get(0);              // remember element to be returned
-            toMove = elements.remove(lastIndex); // element to reheap down
+            hold = elementsArray.get(0);              // remember element to be returned
+            toMove = elementsArray.remove(lastIndex); // element to reheap down
             lastIndex--;                         // decrease priority queue size
             if (lastIndex != -1)
                 reheapDown(toMove);               // restore heap properties
-            return hold;                         // return largest element
+            return hold;                         // return smallest element
         }
     }
-
-
-    private void reheapUp(E element) {
-        int hole = lastIndex;
-        while ((hole > 0) && (element.compareTo(elements.get((hole - 1) / 2)) > 0)) {
-            elements.set(hole,elements.get((hole - 1) / 2));   // move hole's parent down
-            hole = (hole - 1) / 2;                             // move hole up
-        }
-        elements.set(hole, element);                   // place element into final hole
-    }
-
     private void reheapDown(E element) {
         int hole = 0;      // current index of hole
         int newhole;       // index where hole should move to
         newhole = newHole(hole, element);   // find next hole
         while (newhole != hole) {
-            elements.set(hole, elements.get(newhole));  // move element up
+            elementsArray.set(hole, elementsArray.get(newhole));  // move element up
             hole = newhole;                             // move hole down
             newhole = newHole(hole, element);           // find next hole
         }
-        elements.set(hole, element);           // fill in the final hole
+        elementsArray.set(hole, element);           // fill in the final hole
     }
-
     private int newHole(int hole, E element)
     // If either child of hole is larger than element return the index
     // of the larger child; otherwise return the index of hole.
@@ -89,7 +86,7 @@ public class minHeap<E extends Comparable<E>> implements IPriorityQueue<E> {
         else
         if (left == lastIndex)
             // hole has left child only
-            if (element.compareTo(elements.get(left)) < 0)
+            if (element.compareTo(elementsArray.get(left)) < 0)
                 // element < left child
                 return left;
             else
@@ -97,9 +94,9 @@ public class minHeap<E extends Comparable<E>> implements IPriorityQueue<E> {
                 return hole;
         else
             // hole has two children
-            if (elements.get(left).compareTo(elements.get(right)) < 0)
+            if (elementsArray.get(left).compareTo(elementsArray.get(right)) < 0)
                 // left child < right child
-                if (elements.get(right).compareTo(element) <= 0)
+                if (elementsArray.get(right).compareTo(element) <= 0)
                     // right child <= element
                     return hole;
                 else
@@ -107,12 +104,22 @@ public class minHeap<E extends Comparable<E>> implements IPriorityQueue<E> {
                     return right;
             else
                 // left child >= right child
-                if (elements.get(left).compareTo(element) <= 0)
+                if (elementsArray.get(left).compareTo(element) <= 0)
                     // left child <= element
                     return hole;
                 else
                     // element < left child
                     return left;
+    }
+
+    public String toString() {
+        String output="";
+
+        for (int i=0; i<elementsArray.size(); i++) {
+            output += elementsArray.get(i) + " ";
+        }
+
+        return output;
     }
 
 }
---
+++ src/Tests/minHeapTest.java
@@ -0,0 +1,25 @@
+package Tests;
+
+import Model.DataStructures.minHeap;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Matt on 7/23/2017.
+ */
+public class minHeapTest {
+    @Test
+    public void enqueue() throws Exception {
+        minHeap testMinHeap = new minHeap(5);
+
+        testMinHeap.enqueue("a");
+        testMinHeap.enqueue("c");
+        testMinHeap.enqueue("d");
+        testMinHeap.enqueue("b");
+        testMinHeap.enqueue("e");
+
+        System.out.println(testMinHeap.toString());
+    }
+
+}
