f347823a25735d50362c221c1bd0a89a511cc1cb
MMattLeeE <MMattLeeE@gmail.com>
2017-07-25 15:55:07 -0400

    .

---
+++ src/Model/DataStructures/EdgeNode.java
@@ -0,0 +1,9 @@
+package Model.DataStructures;
+
+/**
+ * Created by Matt on 7/25/2017.
+ */
+public class EdgeNode<E> {
+    E destinationVertex;
+    double weight;
+}
---
+++ src/Model/DataStructures/GraphNode.java
@@ -0,0 +1,12 @@
+package Model.DataStructures;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Matt on 7/25/2017.
+ */
+public class GraphNode<E> {
+    E info;
+    ArrayList<E>[] edges;
+
+}
